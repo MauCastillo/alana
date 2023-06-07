35a10de5586a90bce11f58aa38579b69b98ba899
mauro <mauroatm@gmail.com>
2023-06-07 13:21:41 -0500

    init project

---
+++ Shared/HttpRequest/request.go
@@ -0,0 +1,17 @@
+package requests
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+type HttpClient struct{
+	
+}
+
+func (h *HttpClient) Init()error{
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MauCastillo/alana
+
+go 1.20
