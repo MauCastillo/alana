68306446d9ed91b0ba0bce9ec3d0049f9b4b5d51
mauro <mauroatm@gmail.com>
2023-06-10 10:42:39 -0500

    Add function to calculate relative strenght  index
    Include functions and test  to calculate relative strenght index

--- binance-api/technical-analysis/technical-analysis.go
+++ binance-api/technical-analysis/technical-analysis.go
@@ -33,4 +33,43 @@ func CalculateStochasticOscillator(kline []*binance.Kline, period int) float64 {
 	return k * 100
 }
 
+func CalculateRSI(data []*binance.Kline) float64 {
+	var firstPoint float64
+	var secondPointPoint float64
+
+	gains := make([]float64, 0)
+	losses := make([]float64, 0)
+
+
+	// Calculate gains and losses
+	for i := 1; i < len(data); i++ {
+		firstPoint  = convertions.StringToFloat64(data[i].Close)
+		secondPointPoint  = convertions.StringToFloat64(data[i-1].Close) 
+		diff := firstPoint - secondPointPoint
+		if diff >= 0 {
+			gains = append(gains, diff)
+		} else {
+			losses = append(losses, -diff)
+		}
+	}
+
+	// Calculate average gains and losses
+	avgGain := calculateAverage(gains)
+	avgLoss := calculateAverage(losses)
+
+	// Calculate RSI
+	rs := avgGain / avgLoss
+	rsi := 100 - (100 / (1 + rs))
+
+	return rsi
+}
+
+func calculateAverage(data []float64) float64 {
+	sum := 0.0
+	for _, value := range data {
+		sum += value
+	}
+	average := sum / float64(len(data))
+	return average
+}
 
--- binance-api/technical-analysis/technical-analysis_test.go
+++ binance-api/technical-analysis/technical-analysis_test.go
@@ -21,5 +21,23 @@ func TestCalculateStochasticOscillator(t *testing.T) {
 
 
 	k = CalculateStochasticOscillator(tempList, 14) 
-	c.Equal(float64(71.42857142857143), k)
+	c.Equal(float64(0), k)
+}
+
+
+func TestCalculateRSI(t *testing.T) {
+	c := require.New(t)
+
+	tempList := []*binance.Kline{{Close: "50.0"},{Close: "55.0"},{Close: "52.0"},{Close: "48"},{Close: "53"}}
+
+
+	rsi := CalculateRSI(tempList) 
+	c.Equal(float64(58.82352941176471), rsi)
+
+	tempList = []*binance.Kline{{Close: "50.0"},{Close: "55.0"},{Close: "52.0"},{Close: "48"},{Close: "53"},{Close: "57"},{Close: "60"},{Close: "58"},{Close: "62"},{Close: "59"},{Close: "55"},{Close: "50"},{Close: "48"},{Close: "45"}}
+	
+
+
+	rsi = CalculateRSI(tempList) 
+	c.Equal(float64(56.375838926174495), rsi)
 }
